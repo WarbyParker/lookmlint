ee2402c5cd6bd81c9ec8a51db2a2c7729c8237dc
aled-brooks <aled.brooks@warbyparker.com>
2020-01-23 17:01:36 -0500

    Moved _format_output function back to cli.py as this is where it is called.

--- lookmlint/cli.py
+++ lookmlint/cli.py
@@ -7,6 +7,72 @@ from . import lookml
 from . import lookmlint
 
 
+def _format_output(check_name, results):
+    lines = []
+    if check_name == 'label-issues':
+        if 'explores' in results:
+            lines += ['Explores:']
+            for model, model_results in results['explores'].items():
+                lines.append(f'  Model: {model}')
+                for explore, issues in model_results.items():
+                    lines.append(f'    - {explore}: {issues}')
+        if 'explore_views' in results:
+            lines += ['Explore Views:']
+            for model, model_results in results['explore_views'].items():
+                lines.append(f'  Model: {model}')
+                for explore, joins in model_results.items():
+                    lines.append(f'    Explore: {explore}')
+                    for join, issues in joins.items():
+                        lines.append(f'      - {join}: {issues}')
+        if 'fields' in results:
+            lines += ['Fields:']
+            for view, view_results in results['fields'].items():
+                lines.append(f'  View: {view}')
+                for field, issues in view_results.items():
+                    lines.append(f'    - {field}: {issues}')
+    if check_name == 'missing-timeframes':
+        for view, view_results in results.items():
+            lines.append(f'View: {view}')
+            for field, issues in view_results.items():
+                lines.append(f'  Field: {field}')
+                for issue, timeframe in issues.items():
+                    lines.append(f'   - {issue} {timeframe}')
+    if check_name == 'raw-sql-in-joins':
+        for model, model_results in results.items():
+            lines.append(f'Model: {model}')
+            for exploration, joins in model_results.items():
+                lines.append(f'  Explore: {exploration}')
+                for join, sql in joins.items():
+                    lines.append(f'    {join}: {sql}')
+    if check_name == 'unused-includes':
+        for model, includes in results.items():
+            lines.append(f'Model: {model}')
+            for include in includes:
+                lines.append(f'  - {include}')
+    if check_name == 'unused-view-files':
+        for view in results:
+            lines.append(f'- {view}')
+    if check_name == 'views-missing-primary-keys':
+        for view in results:
+            lines.append(f'- {view}')
+    if check_name == 'duplicate-view-labels':
+        for model, model_results in results.items():
+            lines.append(f'Model: {model}')
+            for exploration, joins in model_results.items():
+                lines.append(f'  Explore: {exploration}')
+                for join, num in joins.items():
+                    lines.append(f'    {join}: {num}')
+    if check_name == 'missing-view-sql-definitions':
+        for view in results:
+            lines.append(f'- {view}')
+    if check_name == 'semicolons-in-derived-table-sql':
+        for view in results:
+            lines.append(f'- {view}')
+    if check_name == 'mismatched-view-names':
+        for view_file, view_name in results.items():
+            lines.append(f'- {view_file}: {view_name}')
+    return lines
+
 
 @click.group('cli')
 def cli():
